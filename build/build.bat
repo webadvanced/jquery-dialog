78cbc90883c440064b1f24b4e5f2e25084b00119
Web Advanced <paul.barton@webadvanced.com>
2012-11-01 10:12:10 -0700

    added confirm and dialog helpers

--- build/build.bat
+++ build/build.bat
@@ -3,5 +3,5 @@ set PATH=\
 echo Running jsHint
 C:\Windows\System32\cscript.exe jsHint\wsh.js ..\src\fatBoy.js
 echo Running ajaxMin
-minify\ajaxmin -clobber ..\src\fatBoy.js -o ..\jquery.fatboy.min.js
+minify\ajaxmin -clobber ..\src\dialog.js -o ..\jquery.dialog.min.js
 echo ----------------------------------------------
--- src/js/dialog.js
+++ src/js/dialog.js
@@ -20,7 +20,11 @@
         buildActions,
         buildActionButton,
         init,
-        closeButton;
+        closeButton,
+        confirmEvent,
+        defaultsFromEl,
+        elCheck,
+        opendialogEvent;
 
     buildTitle = function( title ) {
         if( !title ) {
@@ -32,43 +36,138 @@
     };
 
     buildContent = function( content ) {
-    	content = content || "Ooops."
-    	$content.empty().append( content );
+        content = content || "Ooops.";
+        $content.empty().append( content );
     };
 
     buildActions = function( actions ) {
-    	$actions.empty();
+        $actions.empty();
 
-    	//if no actions, add close
-    	if( !actions ) {
+        //if no actions, add close
+        if( !actions ) {
 
             $actions.append( buildActionButton( closeButton ) );
 
-    	} else {
-			
-			var i = 0,
-    			l = actions.length;
+        } else {
+            
+            var i = 0,
+                l = actions.length;
 
-    		for( ; i < l; i++ ) {
-    			$actions.append( buildActionButton( actions[ i ] ) );
-    		}
+            for( ; i < l; i++ ) {
+                $actions.append( buildActionButton( actions[ i ] ) );
+            }
 
-    	}
+        }
     };
 
-	buildActionButton = function( obj ) {
-		
-		if( !obj ) {
-			return;
-		}
+    buildActionButton = function( obj ) {
+        
+        if( !obj ) {
+            return;
+        }
 
-		return $( '<a />', obj );
-	};
+        var btn = $( '<a />', obj );
+        if( obj.callback ) {
+            var ctx = ( obj.ctx ) ? obj.ctx : this;
+            btn.click(function() {
+                obj.callback.apply( ctx, arguments );
+            });
+        }
+
+        return btn;
+    };
+
+    confirmEvent = function( evt ) {
+        evt.preventDefault();
+        var $el = $( this ),
+            obj = { el: this };
+
+        dialog.confirm( defaultsFromEl( $el, obj ) );
+    };
+
+    opendialogEvent = function( evt ) {
+        evt.preventDefault();
+        var $el = $( this ),
+            obj = { el: this },
+            contentKey = $el.attr( 'href' );
+
+        //set options from el
+        defaultsFromEl( $el, obj );
+
+        //check if ajax or inline and get content
+        if( contentKey.charAt( 0 ) === '#') {
+
+            obj.content = $( contentKey ).html();
+            dialog.open( obj );
+
+        } else {
+            $.get( contentKey, function( content ) {
+                obj.content = content;
+                dialog.open( obj );
+            } );
+        }
+    };
 
     closeButton = {
-    	text: 'close',
-    	class: 'primary close',
-    	href: '#close'
+        'text': 'close',
+        'class': 'primary close',
+        'href': '#close'
+    };
+
+    defaultsFromEl = function( $el, obj ) {
+       obj = obj || {};
+
+       elCheck( $el, 'title', obj );
+       elCheck( $el, 'width', obj );
+       elCheck( $el, 'height', obj );
+       elCheck( $el, 'content', obj );
+       elCheck( $el, 'processtext', obj );
+       elCheck( $el, 'canceltext', obj );
+       elCheck( $el, 'processcallback', obj );
+       elCheck( $el, 'cancelcallback', obj );
+
+       obj.action = $el.attr( 'href' );
+
+       return obj;
+    };
+
+    elCheck = function( $el, data, obj ) {
+        if( $el.data( data ) ) {
+               obj[data] = $el.data( data );
+        }    
+    };
+
+    dialog.confirm = function( settings ) {
+        var defaults = {
+            content: 'Are you sure about that?',
+            title: 'please confirm'
+        },
+        options = {},
+        actions = [],
+        cancelBtn = {},
+        processBtn = {};
+        
+        $.extend( options, defaults, settings );
+        
+        //buttons
+        
+        cancelBtn.text = options.canceltext || 'close';
+        cancelBtn.href = '#close';
+        cancelBtn[ 'class' ] = options[ 'class' ] || 'close';
+        cancelBtn.callback = w[ options.cancelcallback ] || undefined;
+        cancelBtn.ctx = options.el || undefined;
+        actions.push( cancelBtn );
+
+        processBtn.text = options.processtext || 'ok';
+        processBtn.href = options.action || '#';
+        processBtn[ 'class' ] = ( options[ 'class' ] || 'danger' ) + ' close';
+        processBtn.callback = w[ options.processcallback ] || undefined;
+        processBtn.ctx = options.el || undefined;
+        actions.push( processBtn );
+
+        options.actions = actions;
+
+        dialog.open( options );
     };
 
     // Center the modal in the viewport
@@ -111,7 +210,7 @@
 
     // Close the modal
     dialog.close = function () {
-    	$modal.hide();
+        $modal.hide();
         $overlay.hide();
         $content.empty();
         $title.empty();
@@ -120,18 +219,20 @@
     };
 
     init = function() {
-    	$main.append( $title, $content, $actions );
-    	$modal.append( $main );
-    	$modal.hide();
+        $main.append( $title, $content, $actions );
+        $modal.append( $main );
+        $modal.hide();
     };
     
     $(function() {
         $( 'body' ).append( $overlay, $modal );
         $( '#modal' ).delegate( '.close', 'click', dialog.close );
+        $( '.confirm' ).live( 'click', confirmEvent );
+        $( '.dialog' ).live( 'click', opendialogEvent );
     });
 
     if( ! w.dialog) {
-        w.dialog = dialog;	
+        w.dialog = dialog;    
     }
 
     init();
