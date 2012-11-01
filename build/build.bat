8c65b806596c66f2309b2314ff62fae480c6dffa
Web Advanced <paul.barton@webadvanced.com>
2012-11-01 10:16:03 -0700

    added css to build script

--- build/build.bat
+++ build/build.bat
@@ -4,4 +4,5 @@ echo Running jsHint
 C:\Windows\System32\cscript.exe jsHint\wsh.js ..\src\js\dialog.js
 echo Running ajaxMin
 minify\ajaxmin -clobber ..\src\js\dialog.js -o ..\dialog\js\jquery.dialog.min.js
+minify\ajaxmin -clobber ..\src\css\dialog.css -o ..\dialog\css\jquery.dialog.css
 echo ----------------------------------------------
