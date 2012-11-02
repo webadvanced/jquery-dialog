e112345823037b9be73eab5f9bc3824cb061797d
Web Advanced <paul.barton@webadvanced.com>
2012-11-02 09:30:27 -0700

    added -o to move min files to site dir

--- build/build.bat
+++ build/build.bat
@@ -5,4 +5,6 @@ C:\Windows\System32\cscript.exe jsHint\wsh.js ..\src\js\dialog.js
 echo Running ajaxMin
 minify\ajaxmin -clobber ..\src\js\dialog.js -o ..\dialog\js\jquery.dialog.min.js
 minify\ajaxmin -clobber ..\src\css\dialog.css -o ..\dialog\css\jquery.dialog.css
+minify\ajaxmin -clobber ..\src\js\dialog.js -o ..\site\js\jquery.dialog.min.js
+minify\ajaxmin -clobber ..\src\css\dialog.css -o ..\site\css\jquery.dialog.css
 echo ----------------------------------------------
